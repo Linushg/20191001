c1d3c88835faa98681dbfcf12c36f3ed858915ad
Linus Gyllenberg <linus@sbgdata.se>
2019-10-01 12:15:55 +0200

    Initial commit

---
+++ CarDealer/CarSale/CarInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSale
+{
+    public class CarInfo
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Regnr { get; set; }
+        public string Kilometer { get; set; }
+        public string Price { get; set; }
+
+    }
+}
